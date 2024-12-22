698c6163db82aba1c70ed42b57fbec5855a7e390
ayushsahu07 <ayushsahu1507@gmail.com>
2024-12-22 19:45:27 +0530

    audio

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let senBtn = document.querySelector("#filter-icon");
 
 let curr = true;
 const mediaQuery = window.matchMedia('(max-width: 768px)');
+let audio = new Audio('Skype call sound.mp3');
+audio.volume = 0.3;
 
 senBtn.addEventListener("click", function () {
     if (curr == true && !mediaQuery.matches) {
@@ -32,23 +34,25 @@ senBtn.addEventListener("click", function () {
 
 
 const openPopup = document.getElementById('new-icon');
-    const closePopup = document.getElementById('close-popup');
-    const popup = document.getElementById('popup');
+const closePopup = document.getElementById('close-popup');
+const popup = document.getElementById('popup');
 
-    // Show the popup
-    openPopup.addEventListener('click', () => {
-        popup.style.display = 'flex';
-    });
+// Show the popup
+openPopup.addEventListener('click', () => {
+    popup.style.display = 'flex';
+    audio.volume = 0.05;
+    audio.play();
+});
 
-    // Close the popup
-    closePopup.addEventListener('click', () => {
+// Close the popup
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none';
+    audio.pause();
+});
+
+// Close popup when clicking outside the content
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
         popup.style.display = 'none';
-    });
-
-    // Close popup when clicking outside the content
-    window.addEventListener('click', (event) => {
-        if (event.target === popup) {
-            popup.style.display = 'none';
-        }
-    });
-
+    }
+});
